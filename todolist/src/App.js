c8ccd04b44ed323b3a3b25a7a55a652c4fb69e37
Tugay Türk <74865603+tugayturk@users.noreply.github.com>
2022-11-05 11:28:38 +0300

    first commit

---
+++ todolist/src/App.js
@@ -0,0 +1,13 @@
+import "./style/style.scss"
+import Button from '@mui/material/Button';
+function App() {
+  return (
+    <div className="App">
+      <div className="title">Title</div>
+      <Button variant="contained">Hello World</Button>
+      Todo
+    </div>
+  );
+}
+
+export default App;
